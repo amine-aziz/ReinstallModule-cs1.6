0ac5a4c7c2f69044f2be272b9e94e51d1c69096a
Amine-Aziz <71028463+Amine-Aziz@users.noreply.github.com>
2021-05-16 12:19:16 +0100

    Update reinstall.sh

--- reinstall.sh
+++ reinstall.sh
@@ -1,6 +1,6 @@
 shopt -s extglob
-echo "vbOEKRhuvEvZRvb" | sudo -S chattr -i hlds_run
-echo "vbOEKRhuvEvZRvb" | sudo -S rm -r !(*.sh)
+chattr -i hlds_run
+rm -r !(*.sh)
 wget http://bachathost.com/cdn/hlds.zip
 unzip hlds.zip
 rm -r hlds.zip
