186845eb5194bb4d6422f4f3928de74b9acd0647
Amine <71028463+Amine-Aziz@users.noreply.github.com>
2023-11-08 00:28:08 +0100

    The reinstall script for cs1.6 linux server fixed for more compatibility.

--- reinstall.sh
+++ reinstall.sh
@@ -1,7 +1,47 @@
-shopt -s extglob
-rm -r !(*.sh)
-wget http://bachathost.com/cdn/hlds.zip
-unzip hlds.zip
-rm -r hlds.zip
-mkdir -p fastdl
-echo "Server reinstalled successfully! Redirecting..."
+#!/bin/bash
+
+# Change the reHLDS link below
+reHLDS_link="http://(reHLDS Link Place)/reHLDS.zip"
+
+# Define the required files and folders
+cstrike="cstrike"
+hlds_linux="hlds_linux"
+hlds_run="hlds_run"
+reHLDS="reHLDS.zip"
+
+# Trying to remove old reHLDS server files
+for file in *; do
+  if [ "$file" != "reinstall.sh" ]; then
+    [ -e "$file" ] && rm -r "$file"
+  fi
+done
+echo "Removing Old Server files.."
+echo "Downloading reHLDS.."
+wget -O "$reHLDS" "$reHLDS_link"
+unzip -o "$reHLDS"
+# Check if server files exist
+if [ -d "$cstrike" ] && [ -f "$hlds_linux" ] && [ -f "$hlds_run" ]; then
+rm -r "$reHLDS"
+echo "Server reinstalled successfully!"
+sleep 1
+echo "Script Created By ! A M 1 N E"
+echo "Get More at : https://github.com/Amine-Aziz/"
+sleep 10
+
+else
+  if [ -f "$reHLDS" ]; then
+  echo "Files mising, reconfiguring files.."
+  unzip -o "$reHLDS"
+    echo "Server reinstalled successfully!"
+  else
+    echo "$reHLDS not found. script cannot complete reinstall."
+  fi
+fi
+sleep 1
+echo "Script Created By ! A M 1 N E"
+echo "Get More at : https://github.com/Amine-Aziz/"
+sleep 10
+
+
+
+
